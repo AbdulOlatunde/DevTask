d9669ac8967636bbb2abc0c3c6de0bf6f85a8832
AbdulOlatunde <olatundeabdullah07@gmail.com>
2025-11-03 08:34:25 -0800

    use dynamic PORT for Leapcell

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.get('/admin/tasks', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Initialize server
